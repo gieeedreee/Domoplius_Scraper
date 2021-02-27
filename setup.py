7484eeba8b39fd9fddd2ee4ed3ee4cd4a29932d7
gieeedreee <66144591+gieeedreee@users.noreply.github.com>
2021-02-28 00:34:47 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='scraper',
+    version='',
+    packages=['test', 'scraper'],
+    url='',
+    license='',
+    author='Giedre',
+    author_email='',
+    description=''
+)
